7e08454cc5b54f888d96e9c9fd36997758e23d20
merry-degaga <merry.degaga@revature.net>
2022-02-08 05:37:18 -0500

    added a video background for login/register page, edited login's ts file to include logic for login / reg form loading

--- project-2-satpoint/src/app/components/login/login.component.ts
+++ project-2-satpoint/src/app/components/login/login.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
+  encapsulation: ViewEncapsulation.None,
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
+  displayReg = false;
+  displayLog = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  display(event:MouseEvent):void{
+    let id = (<HTMLButtonElement>event.target).id;
+    //console.log(id);
+    if(id==="register") {
+      this.displayReg = true;
+      this.displayLog = false;
+    }
+    else if(id==="login") {
+      this.displayLog = true;
+      this.displayReg = false;
+    }
+
   }
 
 }
