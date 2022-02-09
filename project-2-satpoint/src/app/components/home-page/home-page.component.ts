04bb91b3db00ddabaa8bacfbc48613e9c49d85b5
merry-degaga <merry.degaga@revature.net>
2022-02-09 12:40:40 -0500

    resolved conflict

--- project-2-satpoint/src/app/components/home-page/home-page.component.ts
+++ project-2-satpoint/src/app/components/home-page/home-page.component.ts
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-=======
-import { Component, OnInit } from '@angular/core';
 import { LoginService } from 'src/app/services/login.service';
->>>>>>> 5573365ebc11dd8acf9d17f814ce9284a7259d5f
 
 @Component({
   encapsulation: ViewEncapsulation.None,
