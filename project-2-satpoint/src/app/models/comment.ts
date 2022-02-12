368522a627dfdd9834bb81ab3703bd621023e95e
merry-degaga <merry.degaga@revature.net>
2022-02-11 23:20:59 -0500

    added comment class

--- project-2-satpoint/src/app/models/comment.ts
+++ project-2-satpoint/src/app/models/comment.ts
@@ -1,2 +1,10 @@
+import { Sat } from "./sat";
+import { User } from "./user";
+
 export class Comment {
+    constructor(
+        public description:string,
+        public user:User,
+        public sat:Sat
+    ){}
 }
