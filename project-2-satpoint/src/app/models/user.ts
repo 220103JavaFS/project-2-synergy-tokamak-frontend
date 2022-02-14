f8b213a21f82cea95ee149753c35de06a01a9dc0
rogerjarad <rogerjaradgaulke@gmail.com>
2022-02-14 09:33:29 -0800

    registerservice and component

--- project-2-satpoint/src/app/models/user.ts
+++ project-2-satpoint/src/app/models/user.ts
@@ -2,6 +2,7 @@ export class User {
     constructor(
        public userId:number,
        public username:string,
+       public password:string,
        public firstName:string,
        public lastName:string,
        public email:string
